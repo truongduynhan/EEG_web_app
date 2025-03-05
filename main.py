c9b2233cf366f6bb3b5e00b8d98a6fa92648257f
Nhan Duy Truong <duynhan139@gmail.com>
2025-03-05 02:17:33 +0000

    na

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ st.set_page_config(
 fn = 'data/chb01_03.edf'
 if os.path.isfile(fn):
     signals, signal_headers, header = load_raw_data(fn)
-print ('Main: Signal shape {}'.format(signals.shape))
-for signal_header in signal_headers:
-    print (signal_header['label'], signal_header['sample_frequency'])
-print (header)
+    print ('Main: Signal shape {}'.format(signals.shape))
+    for signal_header in signal_headers:
+        print (signal_header['label'], signal_header['sample_frequency'])
+    print (header)
 print ('Done!')
